2dac26387e3cfb8699859a6f357b1fe38cbe477b
Joseph Fula <jfuladotcom@users.noreply.github.com>
2023-07-03 15:40:58 -0400

    Add files via upload

---
+++ cloth/js/json.js
@@ -0,0 +1,44 @@
+
+var P1 = {    
+    title:'Blue Jaws Bite of Comfort',
+    price: "155",
+    img: "imgs/furniture-imgs-03.png" 
+}
+
+var P2 = {    
+    title:'Lay in the Grey',
+    price: "255",
+    img: "imgs/furniture-imgs-01.png"   
+}
+
+var P3 = {    
+    title:'Here comes Sunny days',
+    price: "355",
+    img: "imgs/furniture-imgs-06.png"  
+}
+
+var P4 = {    
+    title:'Sturdy and MAH!',
+    price: "175",
+    img: "imgs/furniture-imgs-04.png" 
+}
+
+var P5 = {    
+    title:'Love & Clarity',
+    price: "325",
+    img: "imgs/furniture-imgs-02.png"   
+}
+
+var P6 = {    
+    title:'Be the Pink',
+    price: "225",
+    img: "imgs/furniture-imgs-05.png"  
+}
+
+var P7 = {    
+    title:'Red or Bed',
+    price: "75",
+    img: "imgs/furniture-imgs-07.png"  
+}
+
+
